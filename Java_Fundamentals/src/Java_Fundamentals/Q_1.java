91152d661c45756082e6a902a8bfe7635772e8c4
Sreedharreddy29 <sreedharr984@gmail.com>
2025-07-10 22:52:57 +0530

    Java Fundamentals

---
+++ Java_Fundamentals/src/Java_Fundamentals/Q_1.java
@@ -0,0 +1,14 @@
+package Java_Fundamentals;
+import java.util.*;
+public class Q_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1+" Technologies "+s2);
+
+	}
+
+}
